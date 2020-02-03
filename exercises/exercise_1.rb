b150023326085d7675cc7567cbae2da0b591eecf
vagrant <aaronpiolang@gmail.com>
2020-02-03 21:52:22 +0000

    completed exercises 1-4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,36 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# CREATE TABLE "stores" 
+# ("id" serial primary key,
+# "name" character varying,
+# "annual_revenue" integer,
+# "mens_apparel" boolean,
+# "womens_apparel" boolean,
+# "created_at" timestamp NOT NULL,
+# "updated_at" timestamp NOT NULL) 
+
+store1 = Store.create(
+    name: "Burnaby",
+    annual_revenue: 300000,
+    mens_apparel: true,
+    womens_apparel: true
+)
+
+store2 = Store.create(
+    name: "Richmond",
+    annual_revenue: 1260000,
+    mens_apparel: false,
+    womens_apparel: true
+)
+
+store3 = Store.create(
+    name: "Gastown",
+    annual_revenue: 190000,
+    mens_apparel: true,
+    womens_apparel: false
+)
+
+puts Store.count()
+
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store1.name = "Vancouver"
+
+@store2 = Store.find_by(id: 2)
+
+puts @store1.name
+puts @store2.name
